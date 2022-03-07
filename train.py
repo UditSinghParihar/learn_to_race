75342b7eec4e40e5d556a66f20b0b679a4e31207
UditSinghParihar <uditsinghparihar96@gmail.com>
2022-03-07 12:53:50 +0530

    Added dummy data and resnet class.

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
 from torchvision import models
 from torch.utils.data import DataLoader, Dataset
 import torch.nn.functional as F
+from torch import nn
+from torch import optim
+import torch
 import argparse
 from tqdm import tqdm
+import numpy as np
 
 
 class L2RDataset(Dataset):
@@ -25,6 +29,14 @@ class L2RDataset(Dataset):
 		"""
 		Fill self.imgs, self.steer and self.throttle
 		"""
+		dummyImage = np.zeros((3, 224, 224))
+		dummySteer = np.array([0.2])
+		dummyThrottle = np.array([0.8])
+
+		self.imgs.append(dummyImage)
+		self.steer.append(dummySteer)
+		self.throttle.append(dummyThrottle)
+		
 		pass
 
 	def __getitem__(self, index):
@@ -34,9 +46,9 @@ class L2RDataset(Dataset):
 
 		data = dict()
 
-		data['img'] = self.imgs[index]
-		data['steer'] = self.steer[index]
-		data['throttle'] = self.throttle[index]
+		data['img'] = torch.from_numpy(self.imgs[index])
+		data['steer'] = torch.from_numpy(self.steer[index])
+		data['throttle'] = torch.from_numpy(self.throttle[index])
 
 		return data
 
@@ -52,12 +64,59 @@ def get_model(args):
 		nn.Tanh()
 	)
 
-	model.to(args.device)
-
 	return model
 
 
-def train_epoch(args, model, dataloader):
+class RegressionNetwork(nn.Module):
+	def __init__(self, args):
+		super().__init__()
+		self.features = models.resnet18(pretrained=True)
+
+		self.regressor = nn.Sequential(
+			nn.Linear(512, 256), 
+			nn.ReLU(),
+			nn.Dropout(0.2),
+			nn.Linear(256, args.num_actions),
+			nn.Tanh()
+		)
+	
+	def normalize_imagenet(self, x):
+		""" Normalize input images according to ImageNet standards.
+		Args:
+			x (tensor): input images
+		"""
+		x = x.clone()
+		x[:, 0] = (x[:, 0] - 0.485) / 0.229
+		x[:, 1] = (x[:, 1] - 0.456) / 0.224
+		x[:, 2] = (x[:, 2] - 0.406) / 0.225
+		
+		return x
+
+
+	def forward(self, x):
+		x = self.normalize_imagenet(x)
+		
+		print("X:", x.shape)
+		# Resnet Layers
+		x = self.features.conv1(x)
+		x = self.features.bn1(x)
+		x = self.features.relu(x)
+		x = self.features.maxpool(x)
+
+		x = self.features.layer1(x)
+		x = self.features.layer2(x)
+		x = self.features.layer3(x)
+		x = self.features.layer4(x)
+
+		x = self.features.avgpool(x)
+		
+		print("After avgpool: ", x.shape)
+		x = self.regressor(x)
+
+		return x
+
+
+def train_epoch(args, model, dataloader, optimizer):
 	model.train()
 
 	# Losses per epoch
@@ -70,22 +129,27 @@ def train_epoch(args, model, dataloader):
 		for p in model.parameters():
 			p.grad = None
 
-		img = torch.from_numpy(data['img']).to(args.device, dtype=torch.float32))
-		steer = torch.from_numpy(data['steer']).to(args.device, dtype=torch.float32))
-		throttle = torch.from_numpy(data['throttle']).to(args.device, dtype=torch.float32))
+		img = data['img'].to(args.device, dtype=torch.float32)
+		steer = data['steer'].to(args.device, dtype=torch.float32)
+		throttle = data['throttle'].to(args.device, dtype=torch.float32)
+
+		# print("Image:", img.shape)
 
 		commands = model.forward(img)
+
+		# print("Commands", commands.shape, commands[0, 0], commands[0, 1])
+		# exit(1)
 
 		# Losses per batch
 		loss = 0.
 		lossSteer = 0.
 		lossThrottle = 0.
 
-		loss += F.l1_loss(commands[0], steer).mean()
-		lossSteer += F.l1_loss(commands[0], steer).mean()
+		loss += F.l1_loss(commands[0, 0], steer).mean()
+		lossSteer += F.l1_loss(commands[0, 0], steer).mean()
 
-		loss += F.l1_loss(commands[1], throttle).mean()
-		lossThrottle += F.l1_loss(commands[1], throttle).mean()
+		loss += F.l1_loss(commands[0, 1], throttle).mean()
+		lossThrottle += F.l1_loss(commands[0, 1], throttle).mean()
 
 		loss.backward()
 		optimizer.step()
@@ -94,9 +158,9 @@ def train_epoch(args, model, dataloader):
 		lossEpochSteer += float(lossSteer.item())
 		lossEpochThottle += float(lossThrottle.item())
 		
-	lossEpoch = lossEpoch / batch_num
-	lossEpochSteer = lossEpochSteer / batch_num
-	lossEpochThottle = lossEpochThottle / batch_num
+	lossEpoch = lossEpoch / (batch_num+1)
+	lossEpochSteer = lossEpochSteer / (batch_num+1)
+	lossEpochThottle = lossEpochThottle / (batch_num+1)
 
 	return lossEpoch, lossEpochSteer, lossEpochThottle
 
@@ -120,7 +184,10 @@ def main():
 
 	args = parser.parse_args()
 
-	model = get_model(args)
+	model = get_model(args).to(args.device)
+	# model = RegressionNetwork(args).to(args.device)
+	# print(model)
+	# exit(1)
 
 	optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
@@ -129,7 +196,7 @@ def main():
 	dataloaderTrain = DataLoader(trainDataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
 
 	for epoch in range(args.epochs):
-		lossEpoch, lossEpochSteer, lossEpochThottle = train_epoch(args, model, dataloaderTrain)
+		lossEpoch, lossEpochSteer, lossEpochThottle = train_epoch(args, model, dataloaderTrain, optimizer)
 
 		# validate_epoch()
 
